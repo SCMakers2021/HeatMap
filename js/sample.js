ae06322ef979ea29553a6276a74c571d29e66aaf
inumaru <inumaru-kazuya@mail.scnet.co.jp>
2021-10-13 09:51:59 +0900

    ベースプロジェクトを作成 以降は好きに開発してOK

--- js/sample.js
+++ js/sample.js
@@ -2,7 +2,7 @@
 var map = {};
 var heatmap;
 var marker,infoWindow,markers=[];
-const HeatMapURL = "https://fj3t6m2cj3.execute-api.us-west-2.amazonaws.com/dev";
+const HeatMapURL = "https://z06c2y3yq8.execute-api.ap-northeast-1.amazonaws.com/dev";
 
 function getMassage(){
   const textbox = document.getElementById("input-message");
@@ -11,8 +11,12 @@ function getMassage(){
 
 function RegisterDB(){
   if(markers != null){
+	// プルダウンからカテゴリを選択
+	let category = document.getElementById('category');
 
     var data = {
+    	function: "InsertData",
+    	category: category.selectedIndex,
         position: markers[0].position,
         summary: markers[0].summary,
     };
@@ -41,19 +45,63 @@ function RegisterDB(){
 
 function ReadDB(){
   if(markers != null){
+	// プルダウンからカテゴリを選択
+	let category = document.getElementById('category');
+
+//	var latUnder = markers[0].position.lat() - 1;
+//	var latUpper = markers[0].position.lat() + 1;
+//	var lngUnder = markers[0].position.lng() - 1;
+//	var lngUpper = markers[0].position.lng() + 1;
+    var posiData = {
+    	latUnder: markers[0].position.lat() - 1,
+    	latUpper: markers[0].position.lat() + 1,
+        lngUnder: markers[0].position.lng() - 1,
+        lngUpper: markers[0].position.lng() + 1
+    };
 
     var data = {
-        position: markers[0].position,
-        summary: markers[0].summary,
+    	function: "ReadData",
+    	category: category.selectedIndex,
+        position: posiData
     };
     
     
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var ele = JSON.stringify({Element: data}, null, ' ');
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: ele,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch(HeatMapURL, requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+  }
+};
+
+//function ReadDB(){
+//  if(markers != null){
+//
+//    var data = {
+//        position: markers[0].position,
+//        summary: markers[0].summary,
+//    };
+//    
+//    
 //    // instantiate a headers object
 //    var myHeaders = new Headers();
 //    // add content type header to object
 //    myHeaders.append("Content-Type", "application/json");
-    // using built in JSON utility package turn object to string and store in a variable
-    var ele = JSON.stringify({Element: data}, null, ' ');
+//  // using built in JSON utility package turn object to string and store in a variable
+//  var ele = JSON.stringify({Element: data}, null, ' ');
 //    // create a JSON object with parameters for API call and store in a variable
 //    var requestOptions = {
 //        method: 'GET',
@@ -61,43 +109,28 @@ function ReadDB(){
 //        body: ele,
 //        redirect: 'follow'
 //    };
-////    $.get(HeatMapURL,
-////      {ele},
-////      function(dt){
-////        console.log(dt);
-////      }
-////    );
-//    fetch(HeatMapURL, requestOptions)
-//    .then(response => response.text())
-//    .then(result => alert(JSON.parse(result).body))
-//    .catch(error => console.log('error', error));
-    
-    const xhr = new XMLHttpRequest();
- 
-//    // 処理の状況変化を監視して自動的に呼ばれる関数
-//    xhr.onreadystatechange = () => {
-//      // readyState XMLHttpRequest の状態 4: リクエストが終了して準備が完了
-//      // status httpステータス
-//      if (xhr.readyState == 4 && xhr.status == 200) {
-// 
-//        if (xhr.response) {
-//        // jsonをオブジェクトに変更
-//        const jsonObj = JSON.parse(xhr.responseText);
-// 
-//        disp(jsonObj, "txt");
-//        }
-// 
-//      }
-//    }
- 
-    // リクエスト
-    xhr.open("GET", HeatMapURL);
- 
-    //リクエスト送信
-    xhr.send(ele);
-
-  }
-};
+//
+//	var latUnder = markers[0].position.lat() - 1;
+//	var latUpper = markers[0].position.lat() + 1;
+//	var lngUnder = markers[0].position.lng() - 1;
+//	var lngUpper = markers[0].position.lng() + 1;
+//    const categoryid   = encodeURIComponent("1");
+//    // URL?category=1&latUnder=35.5&latUpper=36.5&lngUnder=135.1&lngUpper=136.1
+//    var request = HeatMapURL + '?category=' + categoryid
+//     + '&latUnder=' + latUnder + '&latUpper=' + latUpper + '&lngUnder=' + lngUnder + '&lngUpper=' + lngUpper;
+//	fetch(request)
+//	  .then((res)=>{
+//	  	console.log(res);
+//	    return( res.json() );
+//	  })
+//	  .then((json)=>{
+//	    // ここに何らかの処理
+//	    console.log('jsonのルート');
+//	    console.log(json);
+//	  });
+//    
+//  }
+//};
 
 $(function() {
   /* 対象となるタイトルを持ったマーカーの詳細を開く */
@@ -196,6 +229,19 @@ function initialize() {
     });
     
   });
+  
+  const categoryList = 
+          ["寿司", "天ぷら", "おでん"];
+
+   const selectCategoryName = document.getElementById('category');
+   //menuList.disabled = false; //選択可能な状態にする
+   //選択されたジャンルのメニュー一覧に対して処理をする
+   categoryList.forEach((category, index) => {
+     const option = document.createElement('option'); //option要素を新しく作る
+     option.value = index; //option要素の値に、メニューを識別できる番号を指定する
+     option.innerHTML = category; //ユーザー向けの表示としてメニュー名を指定する
+     selectCategoryName.appendChild(option); //セレクトボックスにoption要素を追加する
+   });
 };
 
 function visualize(){
