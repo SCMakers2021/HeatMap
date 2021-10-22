d4dce68ce8edbf57a35c7c27ad504e7e86a325a0
XORveRCOM <contact@xorver.com>
2021-10-22 13:50:05 +0900

    整理とjQueryのバージョンをもとに戻した

--- js/auth.js
+++ js/auth.js
@@ -5,6 +5,7 @@
 $( function() {
 	$( '.js-modal-open' ).each( function() {
 		$( this ).on( 'click', function() {
+			// ログインダイアログを表示させる処理
 			var target = $( this ).data( 'target' );
 			var modal = document.getElementById( target );
 			$( modal ).fadeIn( 300 );
@@ -12,6 +13,7 @@ $( function() {
 		});
 	});
 	$( '.js-modal-close' ).on( 'click', function() {
+		// ウィンドウ外をクリックした時にログインダイアログをクローズさせる処理
 		$( '.js-modal' ).fadeOut( 300 );
 		return false;
 	});
---
+++ js/common.js
@@ -0,0 +1,7 @@
+// @ts-check
+/* jshint esversion: 6, laxbreak: true, laxcomma: true, node: true */
+"use strict";
+
+$( function() {
+	// 共通処理
+});
