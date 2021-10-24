31fcd2f925a4df3443f45670c769f928a9bd1f41
XORveRCOM <contact@xorver.com>
2021-10-25 08:57:34 +0900

    ログイン関係のコントロール

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,6 @@
 // @ts-check
-/* jshint esversion: 6, laxbreak: true, laxcomma: true, node: true */
+/* jshint esversion: 6, laxbreak: true, laxcomma: true, node: true, browser: true, devel: true */
+/* globals $:false */
 "use strict";
 
 $( function() {
@@ -17,5 +18,29 @@ $( function() {
 		$( '.js-modal' ).fadeOut( 300 );
 		return false;
 	});
+	$( '#login-button' ).on( 'click', function() {
+		// ログインをクリックした時にログインさせる処理
+		let email = $('#login-email').val();
+		let pass1 = $('#login-pass').val();
+		// TODO:email チェックも忘れずに
+		// TODO:その他未実装
+		alert("email:" + email + ", pass:" + pass1);
+		return false;
+	});
+	$( '#signup-button' ).on( 'click', function() {
+		// 登録をクリックした時にユーザ登録させる処理
+		let email = $('#signup-email').val();
+		let pass1 = $('#signup-pass').val();
+		let pass2 = $('#signup-pass-repeat').val();
+		// TODO:email チェックも忘れずに
+		// TODO:その他未実装
+		if (pass1==pass2) {
+			alert("succ email:" + email + ", pass:" + pass1);
+		} else {
+			alert("fail email:" + email + ", pass:" + pass1 + ", pass(rep):" + pass2);
+		}
+		return false;
+	});
+
 	$('#login-tab').tabs();
 });
