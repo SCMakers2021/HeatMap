10c6db5bfc94758de0d4536d20ed919ba3afc10d
XORveRCOM <contact@xorver.com>
2021-10-26 08:36:38 +0900

    ユーザ認証コード入力

--- js/auth.js
+++ js/auth.js
@@ -25,6 +25,23 @@ $( function() {
 		// TODO:email チェックも忘れずに
 		// TODO:その他未実装
 		alert("email:" + email + ", pass:" + pass1);
+		// 未認証だった応答の場合に認証コード入力に遷移させる時のコード
+		$('#div-login-input').hide();
+		$('#div-signup-confirm').show();
+		return false;
+	});
+	$( '#signup-confirm-button' ).on( 'click', function() {
+		// 認証コード登録をクリックした時にユーザ登録認証させる処理
+		let text0 = $('#auth-conf-text_0').val();
+		let text1 = $('#auth-conf-text_1').val();
+		let text2 = $('#auth-conf-text_2').val();
+		let text3 = $('#auth-conf-text_3').val();
+		let text4 = $('#auth-conf-text_4').val();
+		let text5 = $('#auth-conf-text_5').val();
+		alert("code:"+text0+text1+text2+text3+text4+text5);
+		// ログイン入力に遷移させる時のコード
+		$('#div-signup-confirm').hide();
+		$('#div-login-input').show();
 		return false;
 	});
 	$( '#signup-button' ).on( 'click', function() {
@@ -36,6 +53,8 @@ $( function() {
 		// TODO:その他未実装
 		if (pass1==pass2) {
 			alert("succ email:" + email + ", pass:" + pass1);
+			// ログインダイアログをクローズさせる処理
+			$( '.js-modal' ).fadeOut( 300 );
 		} else {
 			alert("fail email:" + email + ", pass:" + pass1 + ", pass(rep):" + pass2);
 		}
