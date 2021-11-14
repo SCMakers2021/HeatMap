26f769bf7440084c722f93ecfc2970bb7bc79f87
OtaHisa <otani_hisashi@icloud.com>
2021-11-15 00:10:15 +0900

    ログイン処理の取り込み
    CognitoImplブランチの処理をmasterに取り込みました。
    なお本番用のCognitoのユーザプール及びIDプールの設定に設定値を変更しています。
    詳細はauth.jsのL8-18を参照

--- js/auth.js
+++ js/auth.js
@@ -3,28 +3,126 @@
 /* jshint esversion: 6, laxbreak: true, laxcomma: true, node: true, browser: true, devel: true */
 /* globals $:false */
 
-
 $( function() {
-	function passwordLogin(email, pass) {
-		// TODO:email チェックも忘れずに
-		// TODO:その他未実装
-		alert("email:" + email + ", pass:" + pass);
+	// ユーザープールの設定
+	const poolData = {
+		UserPoolId : 'ap-northeast-1_uynOOPJsV',
+		ClientId : '19aisb95e3mcl8n1oieghs1grq'
+	};
+	const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+	// IDプールの設定
+	// Amazon Cognito 認証情報プロバイダーの初期化
+	AWSCognito.config.region = 'ap-northeast-1'; // リージョン
+	AWSCognito.config.credentials = new AWS.CognitoIdentityCredentials({
+ 	  	IdentityPoolId: 'ap-northeast-1:4c253a26-5a13-47b0-8243-66baf5b2888d'
+	});
+
+	/*
+	AWSCognito.config.credentials.refresh(err => {
+		alert(err);
+	});
+	*/
+
+	/**
+	 * サインアップ
+	 * @param {string} email メールアドレス
+	 * @param {string} password パスワード
+	 * @return {Promise} プロミス
+	 */
+	 function signUp(email, password) {
+		return new Promise(function(onCallback, ngCallback) {
+			userPool.signUp(email, password, null, null, function(err, result) {
+				if (err) {
+					ngCallback(err);
+				} else {
+					onCallback(result);
+				}
+			});
+		});
 	}
 
-	//console.log(getStorage("testParam"));
-	//setStorage("testParam", {"key1": 1});
-	//console.log(getStorage("testParam"));
-	//console.log(removeStorage("testParam"));
+	/**
+	 * サインアップ認証メール再送
+	 * @param {string} email メールアドレス
+	 * @return {Promise} プロミス
+	 */
+	 function signUpResend(email) {
+		return new Promise(function(onCallback, ngCallback) {
+			let userData = {
+				Username: email,
+				Pool: userPool
+			};
+			let cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
-	let l = getStorage("login");
-	if (l!=null) {
-		$('#login-email').val(l.email);
-		$('#login-pass').val(l.pass);
-		$('#login-ishold').prop('checked', true);
-		// TODO:自動ログイン処理
-		// TODO:応答で未認証だったら認証フェーズにする
+			cognitoUser.resendConfirmationCode(function(err, result) {
+				if (err) {
+					ngCallback(err);
+				} else {
+					onCallback(result);
+				}
+			});
+		});
 	}
 
+	/**
+	 * サインアップ認証
+	 * @param {string} email メールアドレス
+	 * @param {string} activationKey 認証コード
+	 * @return {Promise} プロミス
+	 */
+	 function signUpActivation(email, activationKey) {
+		return new Promise(function(onCallback, ngCallback) {
+			let userData = {
+				Username: email,
+				Pool: userPool
+			};
+			let cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+			cognitoUser.confirmRegistration(activationKey, true, function(err, result) {
+				if (err) {
+					ngCallback(err);
+				} else {
+					onCallback(result);
+				}
+			});
+		});
+	}
+
+	/**
+	 * サインイン
+	 * @param {string} email メールアドレス
+	 * @param {string} password パスワード
+	 * @return {Promise} プロミス
+	 */
+	function signIn(email, password) {
+		return new Promise(function(onCallback, ngCallback) {
+			// 認証データの作成
+			let authenticationData = {
+				Username: email,
+				Password: password
+			};
+			let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+			let userData = {
+				Username: email,
+				Pool: userPool
+			};
+			let cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+			// 認証処理
+			cognitoUser.authenticateUser(authenticationDetails, {
+				onSuccess: function (result) {
+					onCallback(new Tokens(result));
+				},
+				onFailure: function(err) {
+					// 直接に ngCallback を書くと分かりづらいため
+					ngCallback(err);
+				}
+			});
+		});
+	}
+
+	// ログインダイアログオープンの有効化
 	$( '.js-modal-open' ).each( function() {
 		$( this ).on( 'click', function() {
 			// ログインダイアログを表示させる処理
@@ -34,43 +132,77 @@ $( function() {
 			return false;
 		});
 	});
+	// ログインダイアログクローズの有効化
 	$( '.js-modal-close' ).on( 'click', function() {
 		// ウィンドウ外をクリックした時にログインダイアログをクローズさせる処理
 		$( '.js-modal' ).fadeOut( 300 );
 		return false;
 	});
+	// タブコントロールの有効化
+	$('#login-tab').tabs();
+
+	// ログイン
 	$( '#login-button' ).on( 'click', function() {
 		// ログインをクリックした時にログインさせる処理
 		let email = $('#login-email').val();
 		let pass1 = $('#login-pass').val();
-		let isHold = $('#login-ishold').prop('checked');
-		passwordLogin(email, pass1);
-		if (isHold) {
+		// ローカルストレージに記録
+		if ($('#login-ishold').prop('checked')) {
 			setStorage("login", {"email": email, "pass": pass1});
 		} else {
 			removeStorage("login");
 		}
-		// TODO:ログイン処理
-		// TODO:応答で未認証だったら認証フェーズにする
-		// 未認証だった応答の場合に認証コード入力に遷移させる時のコード
-		$('#div-login-input').hide();
-		$('#div-signup-confirm').show();
+		// ログイン処理
+		signIn(email, pass1).then((tokens) => {
+			// ログインダイアログをクローズさせる処理
+			$( '.js-modal' ).fadeOut( 300 );
+		}).catch((err) => {
+			if (err.code=='UserNotConfirmedException') {
+				// 応答で未認証だったら認証フェーズにする
+				// 未認証だった応答の場合に認証コード入力に遷移させる時のコード
+				$('#div-login-input').hide();
+				$('#div-signup-confirm').show();
+			} else {
+				alert(err);
+			}
+		});
 		return false;
 	});
+
+	// 新規登録したユーザのアクティベーション
 	$( '#signup-confirm-button' ).on( 'click', function() {
 		// 認証コード登録をクリックした時にユーザ登録認証させる処理
+		let email = $('#login-email').val();
 		let text0 = $('#auth-conf-text_0').val();
 		let text1 = $('#auth-conf-text_1').val();
 		let text2 = $('#auth-conf-text_2').val();
 		let text3 = $('#auth-conf-text_3').val();
 		let text4 = $('#auth-conf-text_4').val();
 		let text5 = $('#auth-conf-text_5').val();
-		alert("code:"+text0+text1+text2+text3+text4+text5);
-		// ログイン入力に遷移させる時のコード
-		$('#div-signup-confirm').hide();
-		$('#div-login-input').show();
+		let activateKey = text0+text1+text2+text3+text4+text5;
+		signUpActivation(email, activateKey).then(result => {
+			// ログイン入力に遷移させる時のコード
+			$('#div-signup-confirm').hide();
+			$('#div-login-input').show();
+		}).catch(err => {
+			alert(err);
+		});
 		return false;
 	});
+
+	// ユーザの新規登録メール再送
+	$( '#resend-button' ).on( 'click', function() {
+		// 登録メール再送をクリックした時に再送させる処理
+		let email = $('#login-email').val();
+		if (email) {
+			signUpResend(email).catch(err => {
+				alert(err);
+			});
+		}
+		return false;
+	});
+
+	// ユーザの新規登録
 	$( '#signup-button' ).on( 'click', function() {
 		// 登録をクリックした時にユーザ登録させる処理
 		let email = $('#signup-email').val();
@@ -79,7 +211,11 @@ $( function() {
 		// TODO:email チェックも忘れずに
 		// TODO:その他未実装
 		if (pass1==pass2) {
-			alert("succ email:" + email + ", pass:" + pass1);
+			signUp(email, pass1).then(res => {
+				alert(res);
+			}).catch(err => {
+				alert(err);
+			});
 			// ログインダイアログをクローズさせる処理
 			$( '.js-modal' ).fadeOut( 300 );
 		} else {
@@ -88,5 +224,30 @@ $( function() {
 		return false;
 	});
 
-	$('#login-tab').tabs();
+	//console.log(getStorage("testParam"));
+	//setStorage("testParam", {"key1": 1});
+	//console.log(getStorage("testParam"));
+	//console.log(removeStorage("testParam"));
+
+	// 「次回から自動的にログインする」の処理
+	let l = getStorage("login");
+	if (l!=null) {
+		$('#login-email').val(l.email);
+		$('#login-pass').val(l.pass);
+		$('#login-ishold').prop('checked', true);
+		// 自動ログイン処理
+		signIn(l.email, l.pass).then((tokens) => {
+			// TODO:応答で未認証だったら認証フェーズにする
+			// 未認証かどうかの検知方法は未定
+		}).catch((err) => {
+			if (err.code=='UserNotConfirmedException') {
+				// 応答で未認証だったら認証フェーズにする
+				// 未認証だった応答の場合に認証コード入力に遷移させる時のコード
+				$('#div-login-input').hide();
+				$('#div-signup-confirm').show();
+			} else {
+				alert(err);
+			}
+		});
+	}
 });
--- js/common.js
+++ js/common.js
@@ -50,6 +50,138 @@
 	}
 }
 
+/**
+ * セッションストレージからデータを取り出す
+ * @param {string} keyName キー
+ * @return {object} 値
+ */
+ function getSession(keyName) {
+	try {
+		let param = sessionStorage.getItem(keyName);
+		return JSON.parse(param);
+	} catch (error) {
+		// file:// のスクリプトで実行したなどでエラー
+		console.log("keyName:" + keyName + ": " + error);
+		return {};
+	}
+}
+
+/**
+ * セッションストレージにデータを設定する
+ * @param {string} keyName キー
+ * @param {object} value 値
+ */
+ function setSession(keyName, value) {
+	try {
+		sessionStorage.setItem(keyName, JSON.stringify(value));
+	} catch (error) {
+		// file:// のスクリプトで実行したなどでエラー
+		console.log("keyName:" + keyName + ": " + error);
+	}
+}
+
+/**
+ * セッションストレージからデータを削除する
+ * @param {string} keyName キー
+ * @return {object} 値
+ */
+ function removeSession(keyName) {
+	try {
+		let oldValue = getStorage(keyName);
+		sessionStorage.removeItem(keyName);
+		return oldValue;
+	} catch (error) {
+		// file:// のスクリプトで実行したなどでエラー
+		console.log("keyName:" + keyName + ": " + error);
+		return {};
+	}
+}
+
+/**
+ * Cognite からのトークン情報
+ */
+class Tokens {
+	constructor(result=null) {
+		this.idToken = "";
+		this.accessToken = "";
+		this.refreshToken = "";
+		if (result) {
+			this.idToken = result.getIdToken().getJwtToken();		  // IDトークン
+			this.accessToken = result.getAccessToken().getJwtToken();  // アクセストークン
+			this.refreshToken = result.getRefreshToken().getToken();   // 更新トークン
+			setSession("tokens", {
+				"idToken": this.idToken
+				, "accessToken": this.accessToken
+				, "refreshToken": this.refreshToken
+			});
+		} else {
+			let obj = getSession("tokens");
+			if (obj.idToken) {
+				this.idToken = obj.idToken;
+				this.accessToken = obj.accessToken;
+				this.refreshToken = obj.refreshToken;
+			}
+		}
+	}
+	/** @return {string} IDトークン */
+	get IdToken() {
+		return this.idToken;
+	}
+	/** @return {string} アクセストークン */
+	get AccessToken() {
+		return this.accessToken;
+	}
+	/** @return {string} 更新トークン */
+	get RefreshToken() {
+		return this.refreshToken;
+	}
+	/** @return {string} サブジェクト */
+	get Sub() {
+		let varSub = "";
+		let value = this.idToken.split('.');
+		if (3 == value.length) {
+			let decoded = atob(value[1]);
+			let valStr = decoded.substr(1, decoded.length -2);
+			let valAry = valStr.split(',');
+			for (let intCnt = 0; intCnt < valAry.length; intCnt++){
+				let varParts = valAry[intCnt];
+				let intRet = varParts.indexOf("sub");
+				if (-1 != intRet){
+					let varCut = varParts.split(':');
+					varSub = varCut[1].substr(1, varCut[1].length -2);
+				}
+			}
+		}
+		return varSub;
+	}
+}
+
+/**
+ * HeatMap API 呼び出し時の fetch をラップして認証情報を付加する
+ * @param {string} url APIのURL
+ * @param {object} options オプション
+ * @return Promise<Response>
+ */
+function apiFetch(url, options) {
+	if (options) {
+		let tokens = new Tokens();
+		if (tokens.Sub) {
+			// TODO: 認証情報
+			// @see https://docs.aws.amazon.com/ja_jp/AmazonS3/latest/userguide/RESTAuthentication.html
+			//    Authorization: AWS AWSAccessKeyId:Signature
+			let auth = tokens.Sub;
+			options.headers.append("Authorization", auth);
+			// @see https://qiita.com/baikichiz/items/ed787c5c79059213401e
+			//    X-Api-Key は API Gateway で API ごとに定義するらしい
+			//    options.headers.append("X-Api-Key", "");
+			// @see https://docs.aws.amazon.com/ja_jp/AmazonS3/latest/userguide/RESTAuthentication.html
+			//    X-Amz-Security-Token は IAM が提供する AWS Security Token
+			//    options.headers.append("X-Amz-Security-Token", "");
+		}
+	}
+	return fetch(url, options);
+}
+
 $( function() {
 	// 共通処理
 });
