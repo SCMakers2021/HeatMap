d0465739d98b76e361227868df6080ae1ea782ac
inumaru <inumaru-kazuya@mail.scnet.co.jp>
2021-11-16 16:33:58 +0900

    位置情報取得失敗時のエラーを追加

--- js/map.js
+++ js/map.js
@@ -92,7 +92,20 @@ function successGeoLocationCallback(position){
 };
 // 現在位置に移動(エラーのコールバック)
 function errorCallback(error){
-  alert("位置情報が取得できませんでした");
+  switch(error.code) {
+    case 1: //PERMISSION_DENIED
+      alert("位置情報の利用が許可されていません");
+      break;
+    case 2: //POSITION_UNAVAILABLE
+      alert("現在位置が取得できませんでした");
+      break;
+    case 3: //TIMEOUT
+      alert("タイムアウトになりました");
+      break;
+    default:
+      alert("その他のエラー(エラーコード:"+error.code+")");
+      break;
+  }
 };
 // 現在位置に移動
 function MoveNowPosition(){
