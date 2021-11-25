6d2371bbc931a7389876bf295e2eeb0363536872
inumaru-kazuya <kazuya.inumaru@gmail.com>
2021-11-26 00:38:49 +0900

    あまり登録時にtwitter投稿する機能を追加。 コメントの変更にも追従。

--- js/twitterObj.js
+++ js/twitterObj.js
@@ -1,32 +1,39 @@
 // tweetボタンを生成
 function CreateTweetButton(){
-    var commnet = $('#commnet').val();
+    var comment = $('#comment').val();
+    if(comment == ""){comment="HitoMapに投稿しました。";}
+
     var tweetButton = document.getElementById("twitter-widget-0");
+    const TweetArea = document.getElementById("TweetButtonArea");
+
     // ボタンは1個だけ。
     if (tweetButton != null) {
-      // すでにボタンがあれば作成せず、メッセージだけ書き換える。
-      var url = tweetButton.src;
-      var textPos = url.match(/text=/);
-      var timePos = url.match(/time=/);
-      var urlText = url.slice(0, textPos.index+5) + commnet + url.slice(timePos.index-1,url.length);
-      console.log(url);
-      console.log(urlText);
-  
-      tweetButton.src = urlText;
-      // ボタンを更新
-      twttr.widgets.load(
-        // document.getElementById("tweetButton")
-        );
-    }else{
+      // // すでにボタンがあれば削除する。
+      if (TweetArea.hasChildNodes()) {
+        TweetArea.removeChild(TweetArea.firstElementChild);
+      }
+    }
+    
+    // ボタンが無ければボタンの追加
+    if (TweetArea.firstElementChild==null) {
+      const tweetButton = document.createElement("a");
+      tweetButton.setAttribute("id", "tweetButton");
+      TweetArea.appendChild(tweetButton);
+    
+      // tweetボタンを生成。
       twttr.widgets.createShareButton(
         "https://master.d3s8lj7u41imf0.amplifyapp.com/",
         document.getElementById('tweetButton'),
         {
-          text: commnet,
+          text: comment,
           hashtags: "HitoMap"
         }
       ).then( function( el ) {
         console.log('Tweet button added.');
+        // 生成後、更新しておく。(これをいれないとtextが更新されなかったため。)
+        twttr.widgets.load(
+          document.getElementById("tweetButton")
+        );
       });
     }
 }
